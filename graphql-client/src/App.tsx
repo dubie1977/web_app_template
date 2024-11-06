e95f04755790045ec1ad99c16c3bebb3f4caf559
Joseph DuBay <dubie1977@gmail.com>
2024-11-05 21:43:59 -0800

    1

--- graphql-client/src/App.tsx
+++ graphql-client/src/App.tsx
@@ -1,15 +1,44 @@
+import * as React from "react";
+import { useState } from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider, Switch } from "@chakra-ui/react";
+
 import "./App.css";
 import FilmList from "./FilmList";
 import StarshipList from "./StarshipList";
 
 function App() {
+  const [showStarships, setShowStarships] = useState(false);
+  const [showFilms, setShowFilms] = useState(false);
+
   return (
-    <div className="App">
-      <h1>Films</h1>
-      <FilmList />
-      <h1>Starships</h1>
-      <StarshipList />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Switch
+          id="films"
+          isChecked={showFilms}
+          onChange={(event) => {
+            console.log(event.target.checked);
+            setShowFilms(!showFilms);
+          }}
+        >
+          Show Films
+        </Switch>
+        {showFilms && <FilmList />}
+        <Switch
+          id="ship"
+          isChecked={showStarships}
+          onChange={(event) => {
+            console.log(event.target.checked);
+            setShowStarships(!showStarships);
+          }}
+        >
+          Show Starships
+        </Switch>
+        {showStarships && <StarshipList />}
+      </div>
+    </ChakraProvider>
   );
 }
 
